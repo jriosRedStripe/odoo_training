e4a2b79dde332e81a3683392aa6d42bb0cbbf73d
Jesús Ríos <jriosa@outlook.com>
2022-09-20 01:08:41 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Academia Odoo',
+    'summary': """ Academy app to manage Training """,
+    'description': """ Academy module to manage Training:
+                * Courses
+                * Sessions
+                *Atendees
+    """,
+    'author': 'Jesús Ríos',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[],
+    'demo':[],
+}
