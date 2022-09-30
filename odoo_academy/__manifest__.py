1a8103d88b13e1e803d2ddc1e9ab808c059997c7
Jesús Ríos <jriosa@outlook.com>
2022-09-30 20:19:14 +0000

    [ADD] Modulo 8

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Academia Odoo',
-    'summary': """ Academy app to manage Training """,
-    'description': """ Academy module to manage Training:
-                * Courses
-                * Sessions
-                * Atendees
-    """,
-    'author': 'Jesús Ríos',
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    
+    'author': 'Odoo',
+    
     'website': 'https://www.odoo.com',
+    
     'category': 'Training',
     'version': '0.1',
+    
     'depends': ['sale'],
-    'data':[
+    
+    'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
         'views/session_views.xml',
-        'views/sale_views_inherit.xml',
+        'views/sales_views_inherit.xml',
         'views/product_views_inherit.xml',
         'wizard/sale_wizard_view.xml',
     ],
-    'demo':[
+    'demo': [
         'demo/academy_demo.xml',
     ],
-    'license': 'OPL-1',
+    #Add license to remove License Warning
+    'license': 'OPL-1'
 }
--- odoo_academy/models/.ipynb_checkpoints/product_template-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    is_session_product = fields.Boolean(string='Use as Session Product',
-                                       help='Check this box to use this as a Product for Session Fee.',
-                                       default=False)
